5332ff61110ea133078aee9598dc6ea78f75b7b7
Alessandro Becci <trofui@gmail.com>
2025-01-17 18:00:31 +0100

    build: modify gradle npm config

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,11 +22,10 @@ gitSemVer {
 }
 
 node {
+    version.set("22.13.0")
+
     // Download a local Node.js distribution (instead of using a global one)
     download.set(true)
-
-    // Pick whichever version of Node.js you prefer
-    version.set("18.17.1")
 
     // If you have a specific version of npm to use, uncomment and set it:
     // npmVersion.set("9.6.6")
@@ -35,21 +34,10 @@ node {
     nodeProjectDir.set(file(project.projectDir))
 }
 
-tasks.register<com.github.gradle.node.npm.task.NpmTask>("installDependencies") {
-    // The command is "npm install"
-    args.set(listOf("install"))
-}
-
 // If you want to run "npm run dev" via Gradle
 tasks.register<com.github.gradle.node.npm.task.NpmTask>("runDev") {
-    dependsOn("installDependencies")
+    dependsOn("npm_install")
     args.set(listOf("run", "dev"))
-}
-
-// If you also want to run "npm start" or anything else, add more tasks:
-tasks.register<com.github.gradle.node.npm.task.NpmTask>("startApp") {
-    dependsOn("installDependencies")
-    args.set(listOf("start"))
 }
 
 tasks.register("printVersion") {
