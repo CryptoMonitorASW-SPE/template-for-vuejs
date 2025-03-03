0f60167a071a2f6e04ffe4fdd87171b727a1730e
Luca Tonelli <luca.tonelli11@studio.unibo.it>
2025-03-03 10:12:35 +0100

    chore: add Vite configuration for Vue.js application

---
+++ app/vite.config.js
@@ -0,0 +1,28 @@
+import { fileURLToPath, URL } from 'node:url'
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueDevTools from 'vite-plugin-vue-devtools'
+
+export default defineConfig({
+  base: '/',
+  publicDir: 'public',
+  plugins: [vue(), vueDevTools()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    emptyOutDir: true,
+    rollupOptions: {
+      input: './index.html'
+    }
+  },
+  server: {
+    port: 8080,
+    strictPort: true,
+    host: true
+  }
+})
