a2372ed047e16b32978fe4bde583494359a79b1a
Luca Tonelli <luca.tonelli11@studio.unibo.it>
2025-03-03 10:09:55 +0100

    feat: migrate from Express to Vue.js framework, adding initial application structure

---
+++ app/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="app">
+    <header>
+      <img
+        alt="GitHub logo"
+        src="https://avatars.githubusercontent.com/u/194215478?s=200&v=4"
+        width="125"
+        height="125"
+      />
+    </header>
+    <main>
+      <HelloWorld />
+    </main>
+  </div>
+</template>
+
+<script>
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+.app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ app/src/components/HelloWorld.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="hello-world">
+    <h1>{{ msg }}</h1>
+    <p>Welcome to your Vue.js application running with Vite</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data() {
+    return {
+      msg: 'Hello World!'
+    }
+  }
+}
+</script>
+
+<style scoped>
+.hello-world h1 {
+  font-size: 2.5rem;
+  color: #42b983;
+}
+
+.hello-world {
+  margin: 0 auto;
+  max-width: 800px;
+}
+</style>
